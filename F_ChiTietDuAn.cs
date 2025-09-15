129dab592d0d6e650586876d5608d8059634d9fa
hoanganhvannham-eng <hoanganhvannham@gmail.com>
2025-09-15 09:43:37 +0700

    fix chi tiet du an

--- F_ChiTietDuAn.cs
+++ F_ChiTietDuAn.cs
@@ -249,20 +249,20 @@ namespace QuanLyNhanVien3
 
 
                 // check ma Nhan vien
-                string checkMaDASql = "SELECT COUNT(*) FROM tblChiTietDuAn  WHERE MaNV  = @MaNV  AND DeletedAt = 0";
-                using (SqlCommand cmdcheckMaDASql = new SqlCommand(checkMaDASql, cn.conn))
-                {
-                    cmdcheckMaDASql.Parameters.AddWithValue("@MaNV", cbMaNV.Text);
-                    int MaHDCount = (int)cmdcheckMaDASql.ExecuteScalar();
+                //string checkMaDASql = "SELECT COUNT(*) FROM tblChiTietDuAn  WHERE MaNV  = @MaNV  AND DeletedAt = 0";
+                //using (SqlCommand cmdcheckMaDASql = new SqlCommand(checkMaDASql, cn.conn))
+                //{
+                //    cmdcheckMaDASql.Parameters.AddWithValue("@MaNV", cbMaNV.Text);
+                //    int MaHDCount = (int)cmdcheckMaDASql.ExecuteScalar();
 
-                    if (MaHDCount != 0)
-                    {
-                        MessageBox.Show("Mã Nhân Viên đã tồn tại trong hệ thống!", "Thông báo",
-                        MessageBoxButtons.OK, MessageBoxIcon.Warning);
-                        cn.disconnect();
-                        return;
-                    }
-                }
+                //    if (MaHDCount != 0)
+                //    {
+                //        MessageBox.Show("Mã Nhân Viên đã tồn tại trong hệ thống!", "Thông báo",
+                //        MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                //        cn.disconnect();
+                //        return;
+                //    }
+                //}
 
 
 
@@ -277,7 +277,7 @@ namespace QuanLyNhanVien3
                 if (confirm == DialogResult.Yes)
                 {
                     cn.connect();
-                    string sql = @"UPDATE tblChiTietDuAn SET TenDA = @TenDA, MoTa = @MoTa,, GhiChu = @GhiChu, DeletedAt = 0 WHERE MaNV = @MaNV";
+                    string sql = @"UPDATE tblChiTietDuAn SET MaDA = @MaDA,Vaitro =@Vaitro ,GhiChu = @GhiChu, DeletedAt = 0 WHERE MaNV = @MaNV";
                     using (SqlCommand cmd = new SqlCommand(sql, cn.conn))
                     {
                         cmd.Parameters.AddWithValue("@MaNV", cbMaNV.SelectedValue);
